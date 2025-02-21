da3d9010de0318cfad20925a528f5f7c6a8bf25b
Liseth-pixel <lisethjazminriverasantos@gmail.com>
2025-02-20 20:06:18 -0600

    Add files via upload

---
+++ carro_2749451/carro_2749451/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro_2749451
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                auto Coche1 = new auto("Mazda", "MX-5 Miata NA", 195, 100);
+                Console.WriteLine("Descripción del vehículo:");
+                Console.WriteLine("Marca: " + Coche1.Marca);
+                Console.WriteLine("Modelo: " + Coche1.Modelo);
+                Console.WriteLine("Velocidad Max: " + Coche1.VelocidadMax);
+                Console.WriteLine("Velocidad Act: " + Coche1.VelocidadAct);
+                Coche1.Acelerar(5);
+                Coche1.Desacelerar(5);
+            }
+        }
+    }
+}
---
+++ carro_2749451/carro_2749451/auto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace carro_2749451
+{
+    public class auto
+    {
+        public auto(string marca, string modelo, int velocidadMax, int velocidadAct)
+        {
+            this.Marca = marca;
+            this.Modelo = modelo;
+            this.VelocidadMax = velocidadMax;
+            this.VelocidadAct = velocidadAct;
+        }
+        public string Marca;
+        public string Modelo;
+        public int VelocidadMax;
+        public int VelocidadAct;
+
+        public void Acelerar(int velocidad)
+        {
+            Console.WriteLine("Acelerando " + velocidad, " km/h");
+        }
+        public void Desacelerar(int velocidad)
+        {
+            Console.WriteLine("desacelerando " + velocidad, " km/h");
+            this.VelocidadAct = velocidad - 5;
+        }
+    }
+}
+//marca, modelo, velocidad máxima, velocidad actual, acelera (de 5 en 5), decelera (de 5 en 5)
+    
